9b4fb63fca15949793ee2389bca99cf2a6996bf0
fwkz <f4wkes@gmail.com>
2018-06-17 20:01:53 +0200

    Add `UDPServiceMock`

--- threat9_test_bed/service_mocks/__init__.py
+++ threat9_test_bed/service_mocks/__init__.py
@@ -2,3 +2,4 @@ from .http_scenario_service import HttpScenarioService  # noqa: F401
 from .http_service_mock import HttpServiceMock  # noqa: F401
 from .tcp_service_mock import TCPServiceMock  # noqa: F401
 from .telnet_service_mock import TelnetServiceMock  # noqa: F401
+from .udp_service_mock import UDPServiceMock  # noqa: F401
